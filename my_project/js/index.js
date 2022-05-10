9bc4434ebc9c54b9e74d677756923c0ae625a034
pritam578@ <pritamkabirajkol@gmail.com>
2022-05-10 11:22:01 +0530

    this is

--- my_project/js/index.js
+++ my_project/js/index.js
@@ -25,5 +25,58 @@ function toggleText2(){
   } else {
     x.style.display = "none";
   }
-
+  }
+ function toggleText3(){
+  var x = document.getElementsByClassName("vid3")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText4(){
+  var x = document.getElementsByClassName("vid4")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText5(){
+  var x = document.getElementsByClassName("vid5")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText6(){
+  var x = document.getElementsByClassName("vid6")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText7(){
+  var x = document.getElementsByClassName("vid7")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText8(){
+  var x = document.getElementsByClassName("vid8")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
   }
