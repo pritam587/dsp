42648783708c7500eb229e937a3c55a977385f39
pritam578@ <pritamkabirajkol@gmail.com>
2022-05-10 20:35:23 +0530

    commit

---
+++ my_project/js/pindex.js
@@ -0,0 +1,82 @@
+function some() {
+	var a=document.querySelector(".con");
+	a.style.color="red";
+}
+function some1() {
+	var a=document.querySelector(".main");
+	a.style.color="red";
+}
+function toggleText1(){
+  var x = document.getElementsByClassName("vid1")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  }
+
+function toggleText2(){
+  var x = document.getElementsByClassName("vid2")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+ function toggleText3(){
+  var x = document.getElementsByClassName("vid3")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText4(){
+  var x = document.getElementsByClassName("vid4")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText5(){
+  var x = document.getElementsByClassName("vid5")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText6(){
+  var x = document.getElementsByClassName("vid6")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText7(){
+  var x = document.getElementsByClassName("vid7")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
+  function toggleText8(){
+  var x = document.getElementsByClassName("vid8")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  }
