6a86e8b5012ee7d06bd12ec3d7ba414bbd7babc2
pritam578@ <pritamkabirajkol@gmail.com>
2022-05-10 10:35:31 +0530

    new commit

---
+++ my_project/js/index.js
@@ -0,0 +1,29 @@
+function some() {
+	var a=document.querySelector(".con");
+	a.style.color="red";
+}
+function some1() {
+	var a=document.querySelector(".main");
+	a.style.color="red";
+}
+function toggleText1(){
+  var x = document.getElementsByClassName("vid1")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  }
+
+function toggleText2(){
+  var x = document.getElementsByClassName("vid2")[0];
+  x.style.display === "none"
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  }
